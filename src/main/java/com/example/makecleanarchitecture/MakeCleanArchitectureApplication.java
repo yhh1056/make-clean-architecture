b2422f02a594a50f2ce95f47bbdcbe46b0a6206e
yhh1056 <yhh1056@naver.com>
2022-11-12 15:35:23 +0900

    first commit

---
+++ src/main/java/com/example/makecleanarchitecture/MakeCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.makecleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makecleanarchitecture/MakeCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makecleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
